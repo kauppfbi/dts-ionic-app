211e477f8821c8b8dea312125007827d541966d0
Fabian K <fabian.kaupp@mail.de>
2019-10-13 10:39:48 +0200

    make challenges the default page

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
-import { TabsPage } from './tabs.page'
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
@@ -13,9 +13,9 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
-          }
-        ]
+              import('../tab1/tab1.module').then(m => m.Tab1PageModule),
+          },
+        ],
       },
       {
         path: 'tab2',
@@ -23,9 +23,9 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-          }
-        ]
+              import('../tab2/tab2.module').then(m => m.Tab2PageModule),
+          },
+        ],
       },
       {
         path: 'tab3',
@@ -33,9 +33,9 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-          }
-        ]
+              import('../tab3/tab3.module').then(m => m.Tab3PageModule),
+          },
+        ],
       },
       {
         path: 'stars',
@@ -43,26 +43,26 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../stars/stars.module').then(m => m.StarsPageModule)
-          }
-        ]
+              import('../stars/stars.module').then(m => m.StarsPageModule),
+          },
+        ],
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
-      }
-    ]
+        redirectTo: '/tabs/tab2',
+        pathMatch: 'full',
+      },
+    ],
   },
   {
     path: '',
     redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
-  }
-]
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class TabsPageRoutingModule {}
