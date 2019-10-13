68476a0707fd21ffc7d5d936279c003318c635d4
Fabian K <fabian.kaupp@mail.de>
2019-10-13 12:10:10 +0200

    adjust layout of leaderboard

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,35 +1,32 @@
-
 import { Component } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
-  constructor(private loadingController: LoadingController) {}
+  constructor() {}
   imageNeuerUrl = '/assets/persons/cacau.png';
 
   leaders: any[] = [
     {
       imageUrl: '/assets/persons/musterfrau2.jpg',
-      name: 'Jovana Morgan',
-      description:  'scored at 16:15',
-      time: '102 km/h',
+      name: 'Kim Jones',
+      description: 'scored at 16:15',
+      time: '99 km/h',
     },
     {
       imageUrl: '/assets/persons/mustermann.jpg',
       name: 'Simon Scott',
-      description:  'scored at 11:30',
-      time: '96 km/h'
+      description: 'scored at 11:30',
+      time: '96 km/h',
     },
     {
       imageUrl: '/assets/persons/kroos.jpg',
       name: 'Toni Kroos',
       description: 'scored at 12:15',
-      time: '95 km/h'
+      time: '95 km/h',
     },
   ];
-
 }
